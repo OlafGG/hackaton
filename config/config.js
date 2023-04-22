f87ea9cb115750ff44af61e1162f7308daf2f5a8
DanielAros <danielaros200@gmail.com>
2023-04-21 18:43:53 -0600

    configuracion de gpt agregada

--- config/config.js
+++ config/config.js
@@ -4,3 +4,5 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+module.exports={openai}
--- controllers/caso.js
+++ controllers/caso.js
@@ -1,17 +1,14 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const {openai} = require('../config/config');
 
 const getCaso = async(req, res) => {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "How are you today?",
       });
-      console.log(completion.data.choices[0].text);
-    res.status(200).json('Iniciar conversacion');
+      const response = completion.data.choices[0].text;
+    res.status(200).json({
+        response
+    });
 }
 
 module.exports = {
