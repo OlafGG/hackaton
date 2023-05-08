27e815a107813608c76e4b1272fb8c64670f4d28
DanielAros <danielaros200@gmail.com>
2023-05-08 09:17:22 -0600

    comentario

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Server = require('./models/server');
+// Esto es un comentario
 
 //llamada al archivo de models server para empezar el proyecto
 const server = new Server();
